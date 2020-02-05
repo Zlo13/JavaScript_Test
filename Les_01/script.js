3785f68f5461a656186adc7b63fbe9cd67c69514
Andrey O <ao13@bk.ru>
2020-02-05 23:13:16 +0300

    Сделал первую часть домашнего задания

--- Les_01/script.js
+++ Les_01/script.js
@@ -1,7 +1,34 @@
-var obj = {
-  name: "Andrey",
-  age: 38,
-  lastName: "Ognivenko"
-};
-console.log(obj.lastName + obj.age);
-document.writeln(obj.name);
+// var obj = {
+//   name: "Andrey",
+//   age: 38,
+//   lastName: "Ognivenko"
+// };
+// console.log(obj.lastName + obj.age);
+// document.writeln(obj.name);
+
+// if (2 * 4 == 7) {
+//   console.log("Все правильно получилось");
+// } else {
+//   console.log("вообще не равно");
+// }
+
+// 2 * 4 == 8
+//   ? console.log("Все правильно получилось")
+//   : console.log("вообще не равно");
+
+let num = 40;
+
+switch (num) {
+  case 40:
+    console.log("мало");
+    break;
+  case num > 50:
+    console.log("много");
+    break;
+  case 50:
+    console.log("то что надо");
+    break;
+  default:
+    console.log("что-то пошло не так");
+    break;
+}
